708f3879f9b1ee27f16703811148bcb6a85366df
Iwark <iwark02@gmail.com>
2014-05-10 23:05:49 +0900

    sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,10 @@
 //= require_tree .
 $(function(){
 
-  $("#menu-toggle").click(function(e) {
+  $(".menu-button").click(function(e) {
     e.preventDefault();
-    $("#wrapper").toggleClass("active");
+    $(".navbar").toggleClass("active");
+    $(".sidebar").toggleClass("active");
   });
 
 });
