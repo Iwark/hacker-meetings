8fb9d5c88845e3f7dd9ad1d6f3b96b75b34e7641
Iwark <iwark02@gmail.com>
2014-05-16 03:16:18 +0900

    authenticate

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -30,13 +30,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
   def authenticate_user!(options={})
-    logger.info("aaa");
-    session[:user_return_to] = env['PATH_INFO']
-    logger.info("aab");
     if !user_signed_in?
+      session[:user_return_to] = env['PATH_INFO']
       redirect_to user_omniauth_authorize_path(:facebook)
-    else
-      super(options)
     end
   end
 
