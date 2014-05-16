87d31bb8f0806dc737b12cf8b728c98c678a46d0
Kohei Iwasaki <iwasaki.kohei@donuts.ne.jp>
2014-05-16 15:40:23 +0900

    admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,12 @@
 class AdminController < ApplicationController
 	before_action :authenticate_user!
+	before_action :authenticate_admin_user!
   def index
-  	
+  	@users = User.all
+  end
+
+  private
+  def authenticate_admin_user!
+  	redirect_to :root if !current_user || current_user.uid != "100001443255301"
   end
 end
